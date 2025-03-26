7193ee97c5ae037c5871c22f40bf2bad5a96f1e0
reolu10 <awomoduireoluwa@gmail.com>
2025-03-26 18:41:11 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import ClothingLandingPage from './ClothingLandingPage';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ClothingLandingPage />
+  </React.StrictMode>
+);
